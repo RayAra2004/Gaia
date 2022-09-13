23f56094745492df8077bf88338988aff8b4bd5f
RayAra2004 <raynan.araujo@gmail.com>
2022-09-13 16:14:42 -0300

    teste2

--- mapsTest.js
+++ mapsTest.js
@@ -1,4 +1,4 @@
-import { CenterControl } from './controles-customizados.js'
+//import { CenterControl } from './controles-customizados.js'
 let map;
 let button = document.querySelector('#mapType');
 let i =0;
@@ -46,6 +46,31 @@ function initMap(){
 
     //map.mapTypes.set('papaTeste', new MapaTeste(new google.maps.Size(256, 256)));
 }
+const centerMap = {lat: -3.716816, lng: -38.519115};
+
+class CenterControl{
+    constructor(map){
+        this.controlDiv = document.createElement('div');
+        this.controlUI = document.createElement('div');
+        this.controlText = document.createElement('div');
+
+        this.controlUI.style.backgroundColor = '#fff';
+        this.controlUI.style.border = '2px solid #ebebeb';
+        this.controlUI.style.borderRadius = '3ox';
+        this.controlUI.style.padding = '6px';
+        this.controlUI.title = 'Centralizar mapa';
+
+        this.controlDiv.appendChild(this.controlUI);
+
+        this.controlText.style.fontSize = '16px';
+        this.controlText.style.textAling = 'center';
+        this.controlText.style.lineHeight = '20px';
+        this.controlText.style.color = '#333';
+        this.controlText.innerHTML = 'Centralizar';
+
+        this.controlUI.appendChild(this.controlText);
+    }
+}
 
 /* map.mapTypes.set(
     "coordinate",
@@ -82,4 +107,3 @@ function initMap(){
 */
 
 // Mapa Teste
-
