ad7b21255099e9d0794579598517664d7d396a84
RayAra2004 <91473437+RayAra2004@users.noreply.github.com>
2022-09-13 16:18:26 -0300

    Update mapsTest.js

--- mapsTest.js
+++ mapsTest.js
@@ -1,4 +1,4 @@
-import { CenterControl } from 'controles-customizados.js'
+//import { CenterControl } from 'controles-customizados.js'
 let map;
 let button = document.querySelector('#mapType');
 let i =0;
@@ -41,8 +41,30 @@ function initMap(){
     const centerControl = new CenterControl(map);
     map.controls[google.maps.ControlPosition.TOP_CENTER].push[centerControl.controlDiv];
     //map.mapTypes.set('papaTeste', new MapaTeste(new google.maps.Size(256, 256))); teste 2
+}
 
+class CenterControl{
+    constructor(map){
+        this.controlDiv = document.createElement('div');
+        this.controlUI = document.createElement('div');
+        this.controlText = document.createElement('div');
 
+        this.controlUI.style.backgroundColor = '#fff';
+        this.controlUI.style.border = '2px solid #ebebeb';
+        this.controlUI.style.borderRadius = '3ox';
+        this.controlUI.style.padding = '6px';
+        this.controlUI.title = 'Centralizar mapa';
+
+        this.controlDiv.appendChild(this.controlUI);
+
+        this.controlText.style.fontSize = '16px';
+        this.controlText.style.textAling = 'center';
+        this.controlText.style.lineHeight = '20px';
+        this.controlText.style.color = '#333';
+        this.controlText.innerHTML = 'Centralizar';
+
+        this.controlUI.appendChild(this.controlText);
+    }
 }
 
 /* map.mapTypes.set(
